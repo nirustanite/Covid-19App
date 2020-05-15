ab9282b007f2da0c5d97867b5a3faaca8898ce98
nirupamaa jaya <nirustanite@gmail.com>
2020-05-15 19:29:01 +0200

    initial project setup with webpack

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class App extends Component{
+    render(){
+        return(
+            <React.Fragment>
+                <h1>Welcome To COVID -19</h1>
+            </React.Fragment>
+        )
+    }
+}
